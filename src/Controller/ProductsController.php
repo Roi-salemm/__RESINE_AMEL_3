e8cff6ccaf548b8f063b4f2397dcab5598f12068
Agastya <sammperso@gmail.com>
2024-01-07 21:35:45 +0100

    Avent tentative d'installation de bundel JWT

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\CategoriesRepository;
 // use App\Entity\Products;
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -48,6 +49,73 @@ class ProductsController extends AbstractController
             'product' => $pro,
         ]);
     }
+
+
+
+
+
+
+    //& Site Raisine
+    //^^ Page boutique - index_prodict.html.twig
+    #[Route('/boutique', name: 'boutique')]
+    public function boutique(ProductsRepository $productsRepository, CategoriesRepository $categoriesRepository){
+
+        // Recherche tout les produits en base
+        $products = $productsRepository->findAll();
+
+        // recherche toutes les categories
+        $categories = $categoriesRepository->findAll();
+
+
+        return $this->render('boutique/index_produit.html.twig', [
+            'name_page' => 'Boutique',
+            'image1' => 'fonctionel.jpeg',
+            'controller_name' => 'titre',
+            'products' => $products,
+            'categories' => $categories,       
+
+        ]);
+    }
+
+        //^^ recherche de produit apres le post
+        #[Route('/boutique/recherche', name: 'boutique_recherche')]
+        public function Boutique_recherche(ProductsRepository $productsRepository, CategoriesRepository $categoriesRepository){
+    
+            // Recherche tout les produits en base
+            $products = $productsRepository->findAll();
+    
+            // recherche toutes les categories
+            $categories = $categoriesRepository->findAll();
+            
+
+            // $form->handleRequest($request);
+
+           
+            // $form = $this->createForm(VotreFormType::class);
+
+            // // Manipulation du formulaire (vérification de la soumission, traitement des données, etc.)
+            // $form->handleRequest($request);
+    
+            // // Vérifie si le formulaire a été soumis et est valide
+            // if ($form->isSubmitted() && $form->isValid()) {
+            //     // Récupération des données du formulaire
+            //     $donnees = $form->getData();
+
+            // }
+
+
+
+        
+            return $this->render('boutique/index_produit.html.twig', [
+                'name_page' => 'Boutique',
+                'image1' => 'fonctionel.jpeg',
+                'controller_name' => 'titre',
+                'products' => $products,
+                'categories' => $categories,       
+    
+            ]);
+        }
+    
 
 
 }
